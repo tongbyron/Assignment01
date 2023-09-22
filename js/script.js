bc140b1a43c90d50deddd6cadc5de111fa6d6ccf
tongbyron <btong@g2ss.com>
2023-09-22 15:40:48 -0700

    initial

---
+++ js/script.js
@@ -0,0 +1,107 @@
+// STEP 1 Camel Case notation
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2
+// numeric literal expression 
+// 1.2
+// string literal expression 
+// "yo"
+// Boolean literal expression
+// true 
+// null literal expression
+// null
+
+// STEP 3 Complex / variable expressions
+// let w = x + y * z
+// let a = (b < c) && (d == e)
+
+// STEP 4 Declare variables using Camel Casing and Hungarian Notation
+// let strFirstName, strLastName, strAddress, strCity, strState, intZipCode, intYourAge, strReferralSource, blnMayWeContactYou
+
+// STEP 5 Declaring and assigning values to variables
+// let strFirstName = 'Byron'
+// let intYourAge = 21 
+// let blnMayWeContactYou = false
+
+// STEP 6 Variable coercion
+// let someString = 21 + ' years old.'
+// console.log(someString)
+
+// STEP 7 Two variables coercion
+// let firstVariable = false + ' positive'
+// console.log(firstVariable)
+// let secondVariable = 21 + true
+// console.log(secondVariable)
+
+// STEP 8 Fix string literal
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// STEP 9 Null / undefined value
+// let nullValue = null
+// console.log(nullValue)
+// let undefinedValue
+// console.log(undefinedValue)
+
+// STEP 10 Unary typeof operator
+// console.log(typeof 'yo')
+// console.log(typeof 1)
+// console.log(typeof false)
+// console.log(typeof null)
+// console.log(typeof undefinedValue)
+
+// STEP 11 Use the concatenation operator to display text in the alert box
+// alert('Hello ' + 'Byron' + ', welcome to the JavaScript class!')
+
+// STEP 12 Declare a variable called name and set it equal to your name
+// let name = 'Byron'
+// alert('Hello ' + name + ', welcome to the JavaScript class!')
+
+// STEP 13 Declare a variable called course and set it equal to “JavaScript”
+// let name = 'Byron', course = 'JavaScript'
+// alert('Hello ' + name + ', welcome to the ' + course + ' class!')
+
+// STEP 14 Add line break
+// let name = 'Byron', course = 'JavaScript'
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+// STEP 15 Replace the hardcoded string of your name with a prompt that asks the user for their name
+// let name = prompt("What is your name?");
+// let course = 'JavaScript'
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+// STEP 16 Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are taking
+// let name = prompt("What is your name?");
+// let course = prompt("What class are you taking?");
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+// STEP 17 Sum of two numbers
+// let x = 10, y = 20
+// console.log(x + y)
+
+// STEP 18 Add and assign
+// let x = 20
+// x += 20
+// console.log(x)
+
+// STEP 19 Multiply and assign
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// STEP 20 Divide and assign
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+// STEP 21 Write an application that evaluates to true
+// let x = 1, y = 2, z= 3
+// console.log((x > y) || (z > y))
+
+// STEP 22 Write an application that evaluates to false
+// let x = 1, y = 2, z= 3
+// console.log((x < y) && (z < y))
